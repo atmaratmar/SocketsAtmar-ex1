31507b30194c22c9f094d98383b73241534e1c51
atma0004 <atma0004@edu.eal.dk>
2017-08-29 11:51:49 +0200

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 20001);
+            server.Start();
+            
+            while (true)
+            {
+                Socket client = server.AcceptSocket();
+                NetworkStream stream = new NetworkStream(client);
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                writer.WriteLine("send to client");
+                Console.WriteLine(reader.ReadLine());
+            }
+           
+
+        }
+    }
+}
---
+++ SocketsAtmar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketsAtmar
+{
+    //Client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("client");
+            TcpClient server = new TcpClient("localhost", 20001);
+            string serverMessage = "";
+            NetworkStream stream = server.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            Console.WriteLine("Message:");
+            serverMessage =reader.ReadLine();
+            Console.WriteLine("Server says: " + serverMessage);
+            writer.WriteLine(Console.ReadLine());
+
+        }
+    }
+}
